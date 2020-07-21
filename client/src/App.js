1573370b5256bc15e03dcbe602138a19ac32b9cb
Shankar Pushparaj <shankarp88@gmail.com>
2020-07-21 18:57:58 -0400

    Pushing Client Server changes

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import FullPageIntroWithFixedNavbar from "./components/FullPageIntroWithFixedNavbar";
+import NavbarFixed from "./components/NavbarFixed";
+import SearchForm from "./components/SearchForm";
+import GroceryItemResults from "./components/GroceryItemResults";
+import AboutUs from "./components/AboutUs";
+import Fridge from "./components/Fridge";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavbarFixed />
+        <Route exact path="/" component={FullPageIntroWithFixedNavbar} />
+        <Route path="/SearchForm" component={SearchForm} />
+        <Route path="/AboutUs" component={AboutUs} />
+        <Route path="/MyFridge" component={Fridge} />
+        <GroceryItemResults />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
