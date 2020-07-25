a583692c6a791942680557de67571ee39dd5bf74
Jada Quandt <jadaquandt@gmail.com>
2020-07-25 13:16:50 -0400

    error fixes

--- client/src/components/RegisterModal.js
+++ client/src/components/RegisterModal.js
@@ -45,9 +45,9 @@ const RegisterModal = (props) => {
 				if (error.response) {
 					alert("Email already exists. Please use a different email.");
 					// Request made and server responded
-					// console.log(error.response.data);
-					// console.log(error.response.status);
-					// console.log(error.response.headers);
+					console.log(error.response.data);
+					console.log(error.response.status);
+					console.log(error.response.headers);
 				} else if (error.request) {
 					// The request was made but no response was received
 					console.log(error.request);
